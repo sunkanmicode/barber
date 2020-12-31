665f3fb582f0c0f93b13f3889016b5950654dbc7
sunkanm <oguntimehins@gmail.com>
2020-12-31 19:17:03 +0100

    updating the project

---
+++ Script.js
@@ -0,0 +1,7 @@
+const sideNav =document.querySelector('.sideNav');
+const manuBtn = document.querySelector('.manuBtn');
+
+manuBtn.addEventListener('click', ()=>{
+  console.log('click');
+  sideNav.classList.toggle('showNav')
+})
