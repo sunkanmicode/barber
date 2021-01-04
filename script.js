6450b15a3bcd08610956f25b6c5fb9f600e57e8d
sunkanmicode <oguntimehins@gmail.com>
2021-01-04 14:48:54 +0100

    remove console.log

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ const sideNav =document.querySelector('.sideNav');
 const manuBtn = document.querySelector('.manuBtn');
 
 manuBtn.addEventListener('click', ()=>{
-  console.log('click');
   sideNav.classList.toggle('showNav')
 });
 
-
-
 sideNav.addEventListener('click', () =>{
      sideNav.classList.remove('showNav')
-})
+});
